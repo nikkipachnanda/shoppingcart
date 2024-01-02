28836f78fb5adb16618f1fa82c7c41916c8d1fdd
nitin <nitinpachnanda@gmail.com>
2024-01-02 21:33:17 +0530

    ui changes

--- frontend/src/screens/Cartscreen.js
+++ frontend/src/screens/Cartscreen.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
 import { FaTrash } from 'react-icons/fa';
-import { Message } from '@mui/icons-material';
+//import { Message } from '@mui/icons-material';
 import { addToCart, removeFromCart } from '../slices/cartSlice';
 
 
@@ -36,9 +36,9 @@ const Cartscreen = () => {
             <Col md={8}>
                 <h1 style={{marginBottom:'20px'}}>Shopping Cart</h1>
                 {cartItems.length ===0 ? (
-                    <Message>
+                    <div>
                         Your cart is empty <Link to="/">Go Back</Link>
-                    </Message>
+                    </div>
                 ):(
                     <ListGroup variant='flush'>
                         {cartItems.map((item)=>(
