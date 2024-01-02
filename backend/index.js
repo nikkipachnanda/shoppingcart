de7dfc297ce9703e5cff6af4a583ad7e64577b3b
nitin <nitinpachnanda@gmail.com>
2024-01-02 21:26:13 +0530

    ui changes

--- backend/index.js
+++ backend/index.js
@@ -36,10 +36,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.get("/", (req, res)=>
-{
-    res.send("APi is running");
-})
+// app.get("/", (req, res)=>
+// {
+//     res.send("APi is running");
+// })
+
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+  } else {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
 
 
 
